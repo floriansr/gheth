140be5173da103f4a55e0e98b8b150281dae9d54
floriansr <46346593+floriansr@users.noreply.github.com>
2020-09-01 19:41:23 +0200

    Model products (#2)
    * [add] server configuration
    * [create] product model
    * [add] express CRUD

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import Product from "./models/Product.js"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
@@ -23,8 +24,39 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use((req, res) => {
-  res.json({ message: "voici le json" });
+app.post("/api/products", (req, res, next) => {
+  delete req.body._id;
+  const product = new Product({
+    ...req.body,
+  });
+  product
+    .save()
+    .then(() => res.status(201).json({ product }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/products/:id", (req, res, next) => {
+  Product.findOne({ _id: req.params.id })
+    .then((product) => res.status(200).json({product}))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.get("/api/products", (req, res) => {
+  Product.find()
+    .then((products) => res.status(200).json({ products }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put('/api/products/:id', (req, res, next) => {
+  Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Modified!' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/products/:id', (req, res, next) => {
+  Product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Deleted!" }))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 export default app;
---
+++ backend/models/Product.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  inStock: { type: Boolean, required: true }
+});
+
+export default mongoose.model("Product", productSchema);
