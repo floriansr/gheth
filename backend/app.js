98e4686b801104074df448e35caf9bf439cbbff2
floriansr <sueurfloriansf@gmail.com>
2020-09-01 18:06:17 +0200

    [add] mongodb connection via mongoose + cors

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,27 @@
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGODB_USERNAME_DB}:${process.env.MONGODB_PASSWORD}@${process.env.CLUSTER_MONGODB}.ozjxx.mongodb.net/<dbname>?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
 
 app.use((req, res) => {
   res.json({ message: "voici le json" });
